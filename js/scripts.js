aaa2db5ddd7cd744ef11d542974c5615d78e9fea
Dawson M <dawson.mortenson@gmail.com>
2017-08-11 15:32:56 -0700

    refactor code to increase readability as well as add a show all data button

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 // BACKEND/BUSINESS LOGIC
+
 // Creates functions to use branching to determine the user's match score
+// Each function assigns points based on what your choice was and if it applied to the given track.
+// If different responses awarded the same track points, it would award a fraction of a point. This
+// gets averaged down and turned into a ratio at the end of the function.
 var determineDesignMatch = function(choice_num1, choice_num2, choice_num3, choice_num4, choice_num5) {
   var match_points = 0;
   var match_score = 0;
@@ -99,6 +103,13 @@ var determineRubyMatch = function(choice_num1, choice_num2, choice_num3, choice_
   return match_score;
 };
 
+// Uses the .max method to find the highest scoring match
+var returnBestMatch = function(match1, match2, match3, match4, match5){
+  var best_match = Math.max(match1, match2, match3, match4, match5);
+  return best_match;
+};
+
+// FRONTEND/USER LOGIC
 // Don't do anything until the document is loaded
 $(document).ready(function() {
   // Wait for a submit event to occur in the form 'user-input'
@@ -111,28 +122,51 @@ $(document).ready(function() {
     var u_age = parseInt($("input:radio[name=user-age]:checked").val());
     var u_enviro = parseInt($("#work-environment").val());
     var u_os = parseInt($("#phone-os").val());
-    // Generate data using the gathered data and store it in vars for increased readability
+
+    // Generates data using the passed parameters and stores it in vars for increased readability
     var css_match = determineDesignMatch(u_type, u_intent, u_age, u_enviro, u_os);
     var net_match = determineNetMatch(u_type, u_intent, u_age, u_enviro, u_os);
     var java_match = determineJavaMatch(u_type, u_intent, u_age, u_enviro, u_os);
     var php_match = determinePhpMatch(u_type, u_intent, u_age, u_enviro, u_os);
     var ruby_match = determineRubyMatch(u_type, u_intent, u_age, u_enviro, u_os);
 
+    // Calls a simple function to improve code readability and keep the backend/frontend separated
+    var best_match_number = returnBestMatch(css_match, net_match, java_match, php_match, ruby_match);
 
-
+    // A little bit of branching in the frontend to determine what should be displayed to the user
+    if(best_match_number === parseInt(css_match)){
+      $("#best-match").text("The CSS/Design Track. You had a " + css_match + "% Match");
+    }else if (best_match_number === parseInt(net_match)) {
+      $("#best-match").text("The C#/.Net Track. You had a "  + net_match + "% Match");
+    }else if (best_match_number === parseInt(java_match)) {
+      $("#best-match").text("The Java/Android Track. You had a "  + java_match + "% Match");
+    }else if (best_match_number === parseInt(php_match)) {
+      $("#best-match").text("The PHP/Drupal Track. You had a "  + php_match + "% Match");
+    }else if (best_match_number === parseInt(ruby_match)) {
+      $("#best-match").text("The Ruby/Rails Track. You had a "  + ruby_match + "% Match.");
+    }else {
+      alert("Error unforseen error detected. Try reloading the page and filling out the survey again.")
+    }
+    // Hides the form
     $("form#user-input").hide();
-    $("#name-results").text(u_name + ",");
-    $("#css-match").text("The CSS/Design Track is a: " + css_match + "% Match");
-    $("#net-match").text("The C#/.Net Track is a: "  + net_match + "% Match");
-    $("#java-match").text("The Java/Android Track is a: "  + java_match + "% Match");
-    $("#php-match").text("The PHP/Drupal Track is a: "  + php_match + "% Match");
-    $("#ruby-match").text("The Ruby/Rails Track is a: "  + ruby_match + "% Match");
-    $("#results").show();
-    // alert(u_name);
-    // alert(u_age)
-    // alert(u_type);
-    // alert(u_intent);
-    // alert(u_enviro);
-    // alert(u_os);
+
+    // Addresses the user
+    $("#name-results").text(u_name + ", based on your answers the best track for you is: ");
+
+    // Shows the user the results
+    $("#results").fadeIn();
+
+    // Sets the text in the all data section in order to use the scope of the '_match' variables
+    $("#css-match").text("CSS/Design Track: " + css_match + "% Match");
+    $("#net-match").text("C#/.Net Track: "  + net_match + "% Match");
+    $("#java-match").text("Java/Android Track: "  + java_match + "% Match");
+    $("#php-match").text("PHP/Drupal Track: "  + php_match + "% Match");
+    $("#ruby-match").text("Ruby/Rails Track: "  + ruby_match + "% Match.");
+  });
+
+  // Once clicked it hides your best match and shows you all the data
+  $("button#show-all").click(function(){
+    $("#results").hide();
+    $("#all-results").fadeIn();
   });
 });
